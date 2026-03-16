9dd22b4b699ede032f41401ca53b68c25ea598a8
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-03-16 13:09:23 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int val1 = 100;
+    printf("int : %d\n", val1);
+    printf("hex : %x\n", val1);
+    printf("oct : %o\n", val1);
 
-    return 0;
+    char val2 = 'c';
+    printf("char : %c\n", val2);
+
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
+
+    float val4 = 3.141592;
+    printf("float : %f\n", val4);
+    printf("exp : %e\n", val4);
 }
