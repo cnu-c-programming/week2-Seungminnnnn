fb933e32811b3880864c86148f92e0a401f322b4
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-03-16 13:15:04 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
-int main()
-{
+int check_prime(int n) {
+    if (n < 2) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+    
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+
+    if (check_prime(n)) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
 
     return 0;
 }
-
