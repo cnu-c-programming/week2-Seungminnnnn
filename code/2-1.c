2b0f5c82a322cac1bcf1920f3f564126944849e9
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-03-16 14:13:36 +0900

    Move float size print statement to a new line

--- code/2-1.c
+++ code/2-1.c
@@ -4,9 +4,10 @@ int main() {
     printf("char: %zu\n", sizeof(char));
     printf("short: %zu\n", sizeof(short));
     printf("int: %zu\n", sizeof(int));
-    printf("float: %zu\n", sizeof(float));
     printf("unsigned int: %zu\n", sizeof(unsigned int));
     printf("long: %zu\n", sizeof(long));
     printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float));
+
     return 0;
 }
