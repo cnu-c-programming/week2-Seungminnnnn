b38057a926cfa38e9491ea0af8e7655d93403a5b
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-03-16 13:14:22 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+
+    int a;
+    int b;
+    char c;
+
+    scanf("%d %d %c", &a, &b, &c);
+
+    if (c == '+') {
+        printf("%d", a + b);
+    } else if (c == '-') {
+        printf("%d", a - b);
+    } else if (c == '*') {
+        printf("%d", a * b);
+    } else if (c == '/') {
+        printf("%d", a / b);
+    }
 
     return 0;
 }
-
